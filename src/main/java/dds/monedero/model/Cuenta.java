a75b7fb3c2a11f171cf45904547c53b712916624
Marco Piatti <marcopiatticastro@hotmail.com>
2022-05-06 17:03:53 -0300

    eliminado codigo repetido en poner y sacar

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -18,39 +18,31 @@ public class Cuenta {
   }
 
   private void acreditarOperacion(double monto, TipoDeMovimiento tipo) {
+    if (monto <= 0) {
+      throw new MontoNegativoException(monto + ": el monto a ingresar debe ser un valor positivo");
+    }
     Movimiento movimiento = new Movimiento(LocalDate.now(), monto, tipo); 
     saldo = movimiento.calcularValor(saldo);
     movimientos.add(movimiento);
   }
 
   public void poner(double monto) {
-    if (monto <= 0) {
-      throw new MontoNegativoException(monto + ": el monto a ingresar debe ser un valor positivo");
-    }
-
     if (movimientos.cantidadDeDepositosEn(LocalDate.now()) >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
-
     acreditarOperacion(monto, TipoDeMovimiento.EXTRACCION);
   }
 
   public void sacar(double monto) {
-    if (monto <= 0) {
-      throw new MontoNegativoException(monto + ": el monto a ingresar debe ser un valor positivo");
-    }
-
     if (saldo - monto < 0) {
       throw new SaldoMenorException("No puede sacar mas de " + saldo + " $");
     }
-
     double montoExtraidoHoy = movimientos.getMontoExtraidoA(LocalDate.now());
     double limite = 1000 - montoExtraidoHoy;
     if (monto > limite) {
       throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + 1000
           + " diarios, límite: " + limite);
     }
-    
     acreditarOperacion(monto, TipoDeMovimiento.EXTRACCION);
   }
 }
